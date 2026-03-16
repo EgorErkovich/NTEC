af341c93499d267fc6ff95233882fb6571725ac3
egorerkovich <erkovich.egor@yandex.by>
2026-03-16 11:25:34 +0300

    refactor: вынесен ключ в .env

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm1f3*4u(!32+$24m2%7$!td)**ou-iy*-#h2pni-)k0ei+!6po'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
