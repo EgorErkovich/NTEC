d55e7348756fde7b87401904367358152fe3428c
egorerkovich <erkovich.egor@yandex.by>
2026-03-16 17:46:43 +0300

    feat: добавлена валидация номера телефона

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "m1f3*4u(!32+$24m2%7$!td)**ou-iy*-#h2pni-)k0ei+!6po")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': os.getenv("DB_NAME", "NTEC_shop"),
         'USER': os.getenv("DB_USER", "ntec_user"),
         'PASSWORD': os.getenv("DB_PASSWORD", "ntec_pass"),
-        'HOST': os.getenv("DB_HOST", "db"),
+        'HOST': os.getenv("DB_HOST", "localhost"),
         'PORT': os.getenv("DB_PORT", "5432"),
     }
 }
